515c2fb118ced0b0f417965bec9b831bff8e5b25
niyahusniyah <59165356+niyahusniyah@users.noreply.github.com>
2019-12-25 23:56:12 +0700

    Add files via upload

---
+++ 2019_12_23_031707_create_mata_kuliahs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMataKuliahsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mata_kuliahs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->char('kd_MK', 9) ->unique();
+            $table->string('nama_MK', 50);
+            $table->char('jmlh_SKS', 6);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mata_kuliahs');
+    }
+}
