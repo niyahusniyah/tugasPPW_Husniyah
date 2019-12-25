432db2d563feb20a6262f4c01c6a4f844973d8a0
niyahusniyah <59165356+niyahusniyah@users.noreply.github.com>
2019-12-25 20:12:20 +0700

    Add files via upload

---
+++ 2019_12_23_031933_create_dosens_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDosensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dosens', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->char('id_dosen', 15) ->unique();
+            $table->string('nama', 150);
+            $table->string('tempat_lahir', 50);
+            $table->date('tgl_lahir');
+            $table->string('alamat');
+            $table->char('jenis_kelamin', 1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dosens');
+    }
+}
